1199ffffbf429aaab4df2234ba67104149e3ec81
suthakaranburaj <suthakaranburaj@gmail.com>
2024-11-03 21:18:17 +0530

    ..

--- Queue Week 12/Lecture2/First -ve integer in every window/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-for(int i=0; i<k; i++ ){
-    //     int ele = q[i];
-    //     if(ele < 0){
-    //         ans.push(i);
-    //     }
-    // }
---
+++ Queue Week 12/Lecture3/firstNonRepeatingCharacterInAStream.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include<queue>
+
+using namespace std;
+void firstNonRepeatingElement(queue<char> &q){
+    queue<char>store;
+    while(!q.empty()){
+
+    }
+}
+int main(){
+    queue<char>q;
+    q.push('a');
+    q.push('b');
+    q.push('c');
+    q.push('d');
+    q.push('e');
+    q.push('f');
+
+    firstNonRepeatingElement(q);
+return 0;
+}
